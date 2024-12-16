39d86c4b1bce3988d8f2886f2b5be542d958e3f4
AyaHdf <aya.hdf24@gmail.com>
2024-12-16 11:34:17 +0100

    paramètre à la méthode hello

--- src/main/java/ma/emsi/houdaifa/appresthoudaifa/HelloResource.java
+++ src/main/java/ma/emsi/houdaifa/appresthoudaifa/HelloResource.java
@@ -2,13 +2,17 @@ package ma.emsi.houdaifa.appresthoudaifa;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 
 @Path("/hello-world")
 public class HelloResource {
+
     @GET
+    @Path("personnes/{nom}")
     @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    public String hello(@PathParam("nom") String nom) {
+        return "Hello, " + nom;
     }
 }
+
